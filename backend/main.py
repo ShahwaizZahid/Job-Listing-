09ae8fec19fbac8b1ca87afce51fc9334e6a5366
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-09-21 16:06:51 +0500

    add: router

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,30 @@
+
+import os
+# from flask import Flask, jsonify
 from flask import Flask
 from flask_cors import CORS
+# from config import config
+# from database import init_db
+# from routes.jobs import jobs_bp
+# from routes.jobs import jobs_routes
+from routes.jobs import jobs_routes
+from flask_sqlalchemy import SQLAlchemy
+from database import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/jobboard'
+db.init_app(app)
+
 CORS(app)
 
 
+app.register_blueprint(jobs_routes) 
 
-# Root route
 @app.route("/", methods=["GET"])
 def hello():
     return {"message": "Hello, Python!"}
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+
+
